4bdaf151a99c5ab883c826bd85d9871d71014e97
freddy campos <fmcampos_new@hotmail.com>
2020-10-22 15:39:52 -0300

    cambios en admin

--- myProyectoCarW/myCar/admin.py
+++ myProyectoCarW/myCar/admin.py
@@ -7,6 +7,11 @@ class SliderIndexAdmin(admin.ModelAdmin):
     search_fields = ['ident']
     list_per_page = 3
 
+class InsumosAdmin(admin.ModelAdmin):
+    list_display = ['nombre','precio','descripcion','stock']
+    search_fields = ['nombre','descripcion']
+    list_per_page = 3
+
 admin.site.register(SliderIndex, SliderIndexAdmin)
 admin.site.register(MisionyVision)
-admin.site.register(Insumos)
+admin.site.register(Insumos, InsumosAdmin)
